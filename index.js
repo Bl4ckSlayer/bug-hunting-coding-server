3302ca132f9b1d6735e92031797bb56a25d643de
Bl4ckSlayer <amirulislamtajbid@gmail.com>
2023-03-19 16:10:16 +0600

    update

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Pass}@cluster0.2kj9s6r.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const usersCollection = client.db("coding-text").collection("sendmail");
+    app.post("/sendmail", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      // TODO: make sure you do not enter duplicate user email
+      // only insert users if the user doesn't exist in the database
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
+
+app.get("/", async (req, res) => {
+  res.send("Server is open");
+});
+app.listen(port, () => {
+  console.log("running", port);
+});
+//
+//
